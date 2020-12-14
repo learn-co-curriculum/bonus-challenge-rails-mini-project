0d70565b43ffa225a8408827bc859326e03d7749
dwisecar <dwisecar@gmail.com>
2020-12-14 15:29:16 -0800

    change name

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## Create Batters and Pitchers and simulate a plate appearance between them.
 
-  
+Start with entering rails s into the terminal. Then enter http://localhost:3000/main to start the webapp.
+
+
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "beats_by_rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
